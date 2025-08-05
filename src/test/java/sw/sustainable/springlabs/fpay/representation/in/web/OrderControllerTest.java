3675d3ddc4eab7cb51b8c7b42501d7d38141279c
jihaneol <6531z@naver.com>
2025-08-05 23:59:28 +0900

    테스트 작성

---
+++ src/test/java/sw/sustainable/springlabs/fpay/representation/in/web/OrderControllerTest.java
@@ -0,0 +1,102 @@
+package sw.sustainable.springlabs.fpay.representation.in.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import sw.sustainable.springlabs.fpay.application.port.in.CreateNewOrderUseCase;
+import sw.sustainable.springlabs.fpay.domain.order.Order;
+import sw.sustainable.springlabs.fpay.representation.request.order.Orderer;
+import sw.sustainable.springlabs.fpay.representation.request.order.PurchaseOrder;
+import sw.sustainable.springlabs.fpay.representation.request.order.PurchaseOrderItem;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@WebMvcTest(controllers = OrderController.class)
+public class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    CreateNewOrderUseCase createNewOrderUseCase;
+
+    @Test
+    @DisplayName("파라미터가 valid를 통과 못할때")
+    public void newOrder() throws Exception {
+
+        // given
+        // 파라미터 객체 생성
+
+        Orderer orderer = new Orderer("", "010-2222-222");
+        List<PurchaseOrderItem> list = Arrays.asList(
+                new PurchaseOrderItem(1, UUID.randomUUID(), "짜파게티", 1000, 2, 2000)
+        );
+
+        PurchaseOrder purchaseOrder = new PurchaseOrder(orderer, list);
+
+        String requestJson = objectMapper.writeValueAsString(purchaseOrder);
+
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/order/new")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("ERROR"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.body.stackTraces").doesNotExist());
+
+        String response = resultActions.andReturn().getResponse().getContentAsString();
+
+        System.out.println(response);
+
+
+    }
+
+    @Test
+    @DisplayName("새로운 주문이 접수가 완료되었을때")
+    public void newOrderSuccess() throws Exception {
+        //given
+        Orderer orderer = new Orderer("박의연", "010-2222-222");
+        List<PurchaseOrderItem> list = Arrays.asList(
+                new PurchaseOrderItem(1, UUID.randomUUID(), "짜파게티", 1000, 2, 2000)
+        );
+
+        PurchaseOrder purchaseOrder = new PurchaseOrder(orderer, list);
+
+        String requestJson = objectMapper.writeValueAsString(purchaseOrder);
+
+        Order order = purchaseOrder.toEntity();
+        order.calculateTotalAmount();
+        order.verifyHaveAtLeastOneItem();
+        order.verifyDuplicateOrderItemId();
+
+        BDDMockito.given(createNewOrderUseCase.createNewOrder(ArgumentMatchers.any()))
+                .willReturn(order);
+
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/order/new")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.body.orderer.name").value("박의연"));
+
+        String result = resultActions.andReturn().getResponse().getContentAsString();
+
+        System.out.println(result);
+
+
+    }
+
+}
